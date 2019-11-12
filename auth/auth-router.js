56a7b0a78db2f00abee69a4029b1ef7912061737
Olga Cortez <olgalataviacortez@gmail.com>
2019-11-12 16:25:48 -0600

    Met MVP for Day 2

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,4 @@
 const bcrypt = require('bcryptjs');
-
 const router = require('express').Router();
 
 const Users = require('../users/users-model.js');
@@ -12,6 +11,7 @@ router.post('/register', (req, res) => {
 
   Users.add(userInformation)
     .then(saved => {
+      req.session.username = saved.username;
       res.status(201).json(saved);
     })
     .catch(error => {
@@ -26,6 +26,7 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.username = user.username;
       res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -34,6 +35,20 @@ router.post('/login', (req, res) => {
     .catch(error => {
       res.status(500).json(error);
     });
+});
+
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(error => {
+      if (error) {
+        res.status(500).json({message: 'Follow the yellow brick road'});
+      } else {
+        res.status(200).json({message: 'Logged out successfully'});
+      }
+    });
+  } else {
+    res.status(200).json({ message: 'Bye!'})
+  }
 });
 
 module.exports = router;
